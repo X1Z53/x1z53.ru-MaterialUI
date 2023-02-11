6dfc657c0e95ed17e1d5b9bdc0d5e22554c20995
X1Z53 <x1z53@yandex.ru>
2023-02-11 18:16:43 +0300

    Fix Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ import { Link, Typography } from "@mui/material"
 
 export default function Footer() {
   return (
-    <footer style={{ marginTop: "auto", paddingTop: "20px", paddingBottom: "10px" }}>
-      <Typography variant="h6" component="footer">X1Z53</Typography>
+    <footer style={{ marginTop: "auto", marginBottom: "10px"}}>
+      <Typography variant="h6">X1Z53</Typography>
       <Link href="https://github.com/X1Z53/site_mui">
-        <GitHubIcon sx={{ color: "text.primary" }} />
+        <GitHubIcon />
       </Link>
     </footer>
   )
