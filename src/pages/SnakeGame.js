596f70ebcca8610b68449ed170d2e834e8b91977
X1Z53 <x1z53@yandex.ru>
2023-02-11 17:48:26 +0300

    1. Refactor Snake Game page 2. Fix packages versions

--- src/pages/SnakeGame.js
+++ src/pages/SnakeGame.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Button, List, Typography } from '@mui/material'
 import Snake from '@yewyewxd/react-simple-snake'
+
+const pageWidth = document.documentElement.scrollWidth
+const pageHeight = document.documentElement.scrollHeight
 
 export default function SnakeGame() {
     return <div>
-        <h1>@yewyewxd/react-simple-snake</h1>
-        <a href="https://www.npmjs.com/package/@yewyewxd/react-simple-snake" target="_blank" rel="noopener noreferrer">NPM</a> -
-        <a href="https://github.com/yewyewxd/react-simple-snake" target="_blank" rel="noopener noreferrer"> Github</a>
-        <p>Use the arrow keys or W/A/S/D to play</p>
-        <Snake />
+        <Typography variant='h4'>@yewyewxd/react-simple-snake</Typography>
+        <List>
+            <Button underline='none' href="https://www.npmjs.com/package/@yewyewxd/react-simple-snake">
+                NPM
+            </Button>
+            <Button underline='none' href="https://github.com/yewyewxd/react-simple-snake">
+                Github
+            </Button>
+        </List>
+        <Snake percentageWidth={pageWidth > pageHeight ? 50 : 75} />
     </div>
 }
